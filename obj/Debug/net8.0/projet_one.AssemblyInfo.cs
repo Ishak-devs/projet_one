496006f2164218188a641c150ff3fa9388c382cd
ishak <i.kouici@insta.fr>
2025-09-16 09:37:18 +0200

    chanement de css pour acceuil

--- obj/Debug/net8.0/projet_one.AssemblyInfo.cs
+++ obj/Debug/net8.0/projet_one.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet_one")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ce94f07a936300de1c106641785a18a354f773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e4cce3412a7ad161f2c7538af5872e1acd4010")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet_one")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet_one")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
