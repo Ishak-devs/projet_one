5b669f79560372a2467d55118d88d1151faa4be7
ishak <i.kouici@insta.fr>
2025-09-17 09:56:52 +0200

    fix buf identity

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace projet_one.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public int Id { get; set; }
 
