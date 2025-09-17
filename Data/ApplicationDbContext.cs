5555491316d6919064a522018b86948c9e56e8fb
ishak <i.kouici@insta.fr>
2025-09-17 10:07:56 +0200

    fix bug identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,11 +4,20 @@ using projet_one.Models;
 
 namespace projet_one.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
-            : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<User>().HasNoDiscriminator();
+        }
     }
 }
